1b385baf348130f56ca19502904740b8e6467071
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-27 12:22:20 -0700

    javascript link creating tests pass

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -1,15 +1,73 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "can create links", :js => :true do
-  scenario "Create a new link" do
-    visit "/"
-    fill_in "Title:", :with => "Turing"
-    fill_in "URL:", :with => "http://turing.io"
-    click_on "Add Link"
+describe "as an authenticated user", :js => :true do
+  context "when I visit the root path" do
+    it "shows me a form to create a link" do
+      user = User.create(email: "test@test.com", password: "test")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    within('#links-list') do
-      expect(page).to have_text("Turing")
-      expect(page).to have_text("http://turing.io")
+      visit root_path
+
+      expect(page).to have_content("Url")
+      expect(page).to have_content("Title")
+    end
+
+    it "can create a new link" do
+      user = User.create(email: "test@test.com", password: "test")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      fill_in "link[url]", with: "Turing"
+      fill_in "link[title]", with: "http://turing.io"
+
+      click_on "Create Link"
+
+      within('#links-list') do
+        expect(page).to have_content(user.links.first.url)
+        expect(page).to have_content(user.links.first.title)
+      end
+    end
+
+    it "cannot let me create a link without a url" do
+      user = User.create(email: "test@test.com", password: "test")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      fill_in "link[url]", with: ""
+      fill_in "link[title]", with: "https://www.google.com"
+
+      click_on "Create Link"
+
+      expect(page).to have_content("Invalid Link")
+    end
+
+    it "cannot let me create a link without a valid url" do
+      user = User.create(email: "test@test.com", password: "test")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      fill_in "link[url]", with: "google.com"
+      fill_in "link[title]", with: "https://www.google.com"
+
+      click_on "Create Link"
+
+      expect(page).to have_content("Invalid Link")
+    end
+
+    it "cannot let me create a link without a title" do
+      user = User.create(email: "test@test.com", password: "test")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      fill_in "link[url]", with: "https://www.google.com"
+
+      click_on "Create Link"
+
+      expect(page).to have_content("Title can't be blank")
     end
 
   end

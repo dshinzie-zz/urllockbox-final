254845a7b68314b09c07136b323f342665727397
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-27 12:25:07 -0700

    add test for links

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -17,10 +17,8 @@ describe "as an authenticated user", :js => :true do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit root_path
-
       fill_in "link[url]", with: "Turing"
       fill_in "link[title]", with: "http://turing.io"
-
       click_on "Create Link"
 
       within('#links-list') do
@@ -34,10 +32,8 @@ describe "as an authenticated user", :js => :true do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit root_path
-
       fill_in "link[url]", with: ""
       fill_in "link[title]", with: "https://www.google.com"
-
       click_on "Create Link"
 
       expect(page).to have_content("Invalid Link")
@@ -64,10 +60,20 @@ describe "as an authenticated user", :js => :true do
       visit root_path
 
       fill_in "link[url]", with: "https://www.google.com"
-
       click_on "Create Link"
 
       expect(page).to have_content("Title can't be blank")
+    end
+
+    it "cannot let me create a link without a url" do
+      user = User.create(email: "test@test.com", password: "test")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+      fill_in "link[title]", with: "Test"
+      click_on "Create Link"
+
+      expect(page).to have_content("Invalid Link")
     end
 
   end

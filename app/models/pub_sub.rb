850fdf62cc3cf6e97da2528154836abe50de3ae2
Daniel Shin <shin.daniel.d@gmail.com>
2017-02-27 21:19:29 -0700

    home run

--- app/models/pub_sub.rb
+++ app/models/pub_sub.rb
@@ -1,12 +1,10 @@
 class PubSub
-  attr_reader :lockbox_to_hotreads_queue, :hotreads_to_lockbox_queue, :hotreads_to_lockbox_queue_top
+  attr_reader :lockbox_to_hotreads_queue
 
   def initialize(connection)
     connection.start
     channel = connection.create_channel
     @lockbox_to_hotreads_queue = channel.queue("ds.lockbox.to.hotreads")
-    @hotreads_to_lockbox_queue = channel.queue("ds.hotreads.to.lockbox")
-
   end
 
   def publish_to_queue(link)
